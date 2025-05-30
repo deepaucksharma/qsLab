65cf1d77d5f8dc1f9d7f95f60404183d414af1f5
007 <you@example.com>
2025-05-31 02:03:02 +0530

    clean

--- learning-app/scripts/docker-aliases.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Docker aliases for WSL when Docker Desktop integration is not enabled
-
-alias docker='docker.exe'
-alias docker-compose='docker-compose.exe'
-
-# Export for use in scripts
-export DOCKER_CMD="docker.exe"
-export DOCKER_COMPOSE_CMD="docker-compose.exe"
